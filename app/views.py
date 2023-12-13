199575be326d63ae10dca084fb5df40169315d1d
Thotakishore5172 <thotakishore5172@gmail.com>
2023-12-13 23:35:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def kishore(request):
+    return render(request,'kishore.html')
+def nani(request):
+    return render(request,'nani.html')
